5eddb88208c9c88539d0d32fb9e41c6a338a9004
teerapon <the_trp@hotmail.com>
2023-07-18 02:34:50 +0700

    Commit NAC

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 
 
-const httpss = 'http://vpnptec.dyndns.org:32001/api'
-const http = 'http://192.168.1.106:32001/api'
+const http = 'http://vpnptec.dyndns.org:32001/api'
+const httpSS = 'http://192.168.1.106:32001/api'
 const httpsss = 'https://5f02-61-7-147-129.ngrok-free.app/api'
 
 
