9471c2c2140c56f705de26667ab733739c34d085
teerapon <the_trp@hotmail.com>
2023-08-29 16:43:04 +0700

    COMMIT NAC 29/08/2023 16:42

--- src/PAGE_NAC/Menu/NAC/nac_create/nac_Seals/nac_seals_approve.js
+++ src/PAGE_NAC/Menu/NAC/nac_create/nac_Seals/nac_seals_approve.js
@@ -1990,10 +1990,11 @@ export default function Nac_Main() {
                           ) : null}
                           {(sendHeader[0].nac_status === 2 && approveData.filter((res) => res.approverid === data.UserCode)[0]) ||
                             (sendHeader[0].nac_status === 3 && approveData.filter((res) => res.approverid === data.UserCode)[0]) ||
+                            (sendHeader[0].nac_status === 5 && permission_MenuID.indexOf(11) >= 0) ||
                             (sendHeader[0].nac_status === 15 && permission_MenuID.indexOf(11) >= 0) ||
                             (sendHeader[0].nac_status === 11 && permission_MenuID.indexOf(11) >= 0) ||
-                            ((sendHeader[0].nac_status === 2 || sendHeader[0].nac_status === 3 || sendHeader[0].nac_status === 15 || sendHeader[0].nac_status === 11) &&
-                              permission_MenuID.indexOf(10) >= 0) ? (
+                            ((sendHeader[0].nac_status === 2 || sendHeader[0].nac_status === 3 || sendHeader[0].nac_status === 15 || sendHeader[0].nac_status === 11 || sendHeader[0].nac_status === 5)
+                              && permission_MenuID.indexOf(10) >= 0) ? (
                             <Stack>
                               <Button
                                 variant="contained"
